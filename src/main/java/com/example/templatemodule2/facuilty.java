546fa10f250ebf11fe7c2ad0573db06a66d6a4d0
Aidan <abenton@uoguelph.ca>
2025-03-03 21:21:23 -0500

    baseCode

---
+++ src/main/java/com/example/templatemodule2/facuilty.java
@@ -0,0 +1,11 @@
+package com.example.templatemodule2;
+
+public class facuilty {
+    String name;
+    char degree;
+    String rInterest;
+    int[] coursesOffered = new int[10];
+    String email;
+    String officeLocation;
+    int password;
+}
